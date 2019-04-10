607b7ce82b72fece3c1a4f81d7e41119b425d692
Wei_da <da.11@osu.edu>
2019-04-10 18:59:55 -0400

    receive and send for sync

--- KiwiBuzzer/Program.cs
+++ KiwiBuzzer/Program.cs
@@ -15,67 +15,63 @@ namespace KiwiBuzzer
     {
         
         // This is used as a header for the packet payload to identify the app
-        private const string Header = "PingPong";
-
-        // The current value
-        static int _currVal;
+        private const static string HeaderRequest = "Request";
+        private const static string HeaderRespond= "Respond";
+        private const static int HeadLength = System.Math.Max(HeaderRespond.Length, HeaderRequest.Length);
 
         private static MACBase _macBase;
+
+        static int _N = 0;
+        static long _offset = 0;
         
         public static void Main()
         {
 	        while(true)
 	        {
-		        
 	        }
+        }
+
+        private static long time2Long(DateTime dateTime) {
+            return (dateTime.ToUniversalTime().Ticks - 621355968000000000) / 10000;
         }
 
         private static void RadioReceive(IMAC macBase, DateTime receiveDateTime, Packet packet)
         {
+            long sentTime = time2Long(packet.SenderEventTimeStamp); // t1 for requesting message, t3 for response message
+            long recvTime = time2Long(receiveDateTime); // t4 for cacluate time offset, t2 for respond
             Debug.Print("Received " + packet.Payload.Length + " bytes from " + packet.Src);
-
-            // Check if message is for us
             var msgByte = packet.Payload;
             var msgChar = Encoding.UTF8.GetChars(msgByte);
             var msgStr = new string(msgChar);
-            if (msgStr.Substring(0, Header.Length) != Header)
+            if (msgStr.Substring(0, HeadLength) == HeaderResponse)
             {
-                return;
-            }
-            // Get payload and check if it is in the correct format (an integer)
-            string payload = msgStr.Substring(Header.Length);
-            int recVal;
-            try
-            {
-                recVal = int.Parse(payload);
-            }
-            catch
-            {
-                return;
-            }
+                string payload = msgStr.Substring(Header.Length);
+                String[] timeStrings = payload.Split(" ");
+                long requstTime, recvRequestTime;
+                long respondTime = sentTime; 
+                long recvResponseTime = recvTime;
+                try
+                {
+                    requstTime = long.Parse(timeStrings[0]);
+                    recvRequestTime = long.Parse(timeStrings[1]);
+                }
+                catch
+                {
+                    return;
+                }
 
-            //
-            // We've received a correct message
-            //
-
-            // Reset the no-response timer
-            StartOneshotTimer(ref _noResponseDelayTimer, NoResponseDelayTimerCallback, NoResponseInterval);
-
-            // Update the current value
-            int origVal = _currVal;
-            _currVal = System.Math.Max(_currVal, recVal);
-            _currVal++;
-            Lcd.Write(_currVal);
-            Debug.Print("Orig val " + origVal + ", rec val " + recVal + ", new val " + _currVal);
-            Debug.Print("\nrssi: " + packet.RSSI);
-
-            // Wait a bit before sending reply
-            StartOneshotTimer(ref _replyTimer, ReplyTimerCallback, SendInterval);
+                _N++;
+                long rtt = (recvResponseTime  - requstTime) - (respondTime - recvRequestTime);
+                _offset = (offset * (N - 1) + (recvRequestTime - requstTime) - (rtt / 2)) / N;
+            } else if (msgStr.Substring(0, HeaderLength) == HeaderRequest) {
+                RadioSend(sentTime.toString() + " " + recvTime.toString(), packet.Src);
+            } 
+            return;
         }
 
         private static void RadioSend(string toSend)
         {
-            var toSendByte = Encoding.UTF8.GetBytes(Header + toSend);
+            var toSendByte = Encoding.UTF8.GetBytes(HeaderRequest + toSend);
             var neighborList = MACBase.NeighborListArray();
             _macBase.NeighborList(neighborList);
             foreach (var theNeighbor in neighborList)
@@ -84,9 +80,16 @@ namespace KiwiBuzzer
                 {
                     break;
                 }
-                Debug.Print("Sending message \"" + toSend + "\" to " + theNeighbor);
-                _macBase.Send(theNeighbor, toSendByte, 0, (ushort)toSendByte.Length);
+                Debug.Print("Sending request message  \"" + toSend + "\" to " + theNeighbor);
+                _macBase.Send(theNeighbor, toSendByte, 0, (ushort)toSendByte.Length, DateTime.Now);
             }
+        }
+
+        private static void RadioSend(string toSend, ushort address)
+        {
+            var toSendByte = Encoding.UTF8.GetBytes(HeaderRespond + toSend);
+            Debug.Print("Sending response message \"" + toSend + "\" to " + address);
+            _macBase.Send(address, toSendByte, 0, (ushort)toSendByte.Length, DateTime.Now);
         }
     }
 }
